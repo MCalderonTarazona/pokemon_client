73e8acc4f4eeb45ccf94a95e44b618f4f13e42d4
MCalderonTarazona <mact.11@gmail.com>
2023-04-30 12:11:08 -0500

    Frontend Initial Login/Register

--- api/src/controllers/loginControllers.js
+++ api/src/controllers/loginControllers.js
@@ -6,17 +6,17 @@ const login = async (req, res) => {
         if (email && password) {
             const emailFind = await User.findOne({ where: { email: email } });
                 if (!emailFind) {
-                    res.status(404).send("Usuario no encontrado");
+                    res.status(404).json({msg: "User does not exist"});
                 } else {
                     const passFind = await User.findOne({ where: { email: email, password: password } });
                     if (!passFind) {
-                        res.status(404).send("Contraseña incorrecta"); 
+                        res.status(404).json({msg: "Incorrect password"});
                     } else {
                         res.status(200).json({access: true})
                     }
                 }   
         } else {
-            res.status(404).send("Faltan datos para la validación");
+            res.status(404).json({msg: "Missing data for validation"})
         }
         
     } catch (error) {
--- api/src/controllers/userControllers.js
+++ api/src/controllers/userControllers.js
@@ -4,22 +4,23 @@ const {Role} = require("../db");
 const createUser = async (req, res) => {
     try {
         const {name, email, password } = req.body;
+        console.log(req.body);
         if( !name || !email || !password ){
-            return res.status(404).send('Faltan datos para el registro');
+            return res.status(404).json({msg: "Missing data for registration"});
         }
 
         const [newUser, created] = await User.findOrCreate({
             where: { name, email, password },
         });
 
-        if (!created){ res.status(404).send('El usuario ya existe');
+        if (!created){ res.status(404).json({msg: "User already exists"});
         }else{
             await newUser.setRole(2);
-            res.status(200).send('El usuario se ha creado con exito');  
+            res.status(200).json({access: true});  
         }
         
     } catch (error) {
-        res.status(500).send(error.message);
+        res.status(500).json({access: false});
     }
     
 }
